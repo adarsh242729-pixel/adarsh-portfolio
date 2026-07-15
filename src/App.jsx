c9473c645ff110f712af3228e7d2cc8ee7ef0846
adarsh242729-pixel <adarsh242729@gmail.com>
2026-07-15 14:28:49 +0530

    feat: Add analytics tracking to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Portfolio from './components/Adarsh_Singh_Portfolio.jsx'
 
 export default function App(){ 
+  useEffect(() => {
+    // Track page view
+    if (window.gtag) {
+      window.gtag('event', 'page_view', {
+        page_title: document.title,
+        page_location: window.location.href
+      });
+    }
+  }, []);
+
   return <Portfolio />
 }
